4aa192edcd61d3c25e6b3ab6d432b09dc8696ba2
Sophie Tagar <sophie24587@gmail.com>
2018-05-01 12:46:48 -0700

    Update main.java

--- Branches/src/main/main.java
+++ Branches/src/main/main.java
@@ -1,13 +1,10 @@
 package main;
 
-import java.awt.List;
 import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.net.URI;
 import java.net.URISyntaxException;
+import java.net.URL;
 import java.util.ArrayList;
 
-import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.ClientProtocolException;
@@ -16,21 +13,14 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
-import org.glassfish.jersey.client.RequestProcessingInitializationStage;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
-import com.applitools.eyes.BatchInfo;
-import com.applitools.eyes.FileLogger;
 import com.applitools.eyes.RectangleSize;
-import com.applitools.eyes.StdoutLogHandler;
 import com.applitools.eyes.selenium.Eyes;
 import com.applitools.eyes.selenium.StitchMode;
-import com.applitools.eyes.selenium.fluent.Target;
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;   
 
 public class main {
 	
