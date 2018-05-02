b6ff7202ef416d3133825014ba1cd055a13cac46
Sophie Tagar <sophie24587@gmail.com>
2018-05-01 17:17:16 -0700

    Update main.java

--- Branches/src/main/main.java
+++ Branches/src/main/main.java
@@ -1,68 +1,62 @@
-package main;
+package Package;
 
-import java.io.IOException;
-import java.net.URISyntaxException;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.ArrayList;
 
-import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.message.BasicNameValuePair;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.junit.Before;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 import com.applitools.eyes.BatchInfo;
 import com.applitools.eyes.RectangleSize;
 import com.applitools.eyes.selenium.Eyes;
-import com.applitools.eyes.selenium.StitchMode;
 
-public class main {
+
+class Test {
+	public Eyes eyes = new Eyes();
+	public String AppName = "GiyhubIntegration";
+	public String TestName = "Test_Github_Integration";  
+	RemoteWebDriver driver;
 	
-	private static Eyes eyes = new Eyes();
-	private static String AppName = "GiyhubIntegration";
-	private static String TestName = "Test_Github_Integration";  
-
-	public static void main(String[] args) throws URISyntaxException, ClientProtocolException, IOException {
-		// TODO Auto-generated method stub
+    @BeforeEach
+    public void initialize() throws MalformedURLException {
 		System.out.println("--------------test-----------------");
+		System.err.println(System.getenv("APPLITOOLS_API_KEY"));
 		eyes.setApiKey(System.getenv("APPLITOOLS_API_KEY"));
 		final String sauceUser = System.getenv("SAUCE_USER");
 		final String sauceKey = System.getenv("SAUCE_KEY");
 		String url = "http://" + sauceUser + ":" + sauceKey + "@ondemand.saucelabs.com:80/wd/hub";
 		
+		
 		DesiredCapabilities caps = DesiredCapabilities.chrome();
 		caps.setCapability("platform", "Windows 10");
 		caps.setCapability("version", "65.0");
 
-		RemoteWebDriver driver = new RemoteWebDriver(new URL(url), caps);
+		driver = new RemoteWebDriver(new URL(url), caps);
 		
 		String batchId   = System.getenv("APPLITOOLS_BATCH_ID");
+		System.out.println(System.getenv("APPLITOOLS_BATCH_ID"));
 		String batchName = "BatchName";
 		BatchInfo batchInfo = new BatchInfo(batchName); 
 		batchInfo.setId(batchId);
 		eyes.setBatch(batchInfo);
-		MainBranch(driver);
-
-	}
-
-	private static void MainBranch(WebDriver driver) {
-		
+     }
+    
+	@org.junit.jupiter.api.Test
+	void test() {
 		try {
+			System.out.println("in test method");
 			eyes.open(driver, AppName, TestName, new RectangleSize(900, 600));
 			driver.get("https://applitools.com/helloworld/");
-			eyes.checkWindow();
-			eyes.setStitchMode(StitchMode.CSS);
 			
-
-			eyes.close(false);
-			System.out.println("Main Branch");
+			eyes.checkWindow();
+			System.out.println("in test method 2");
+			eyes.close();
+//			System.out.println("Main Branch");
 
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
@@ -70,6 +64,6 @@ public class main {
 			driver.quit();
 			eyes.abortIfNotClosed();
 		}
-
 	}
+
 }
