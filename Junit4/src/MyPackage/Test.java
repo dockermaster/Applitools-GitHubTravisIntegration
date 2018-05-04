23145ce920d1b0588254849801658a00efc361b3
Sophie Tagar <sophie24587@gmail.com>
2018-05-03 17:15:31 -0700

    Update Test.java

--- Junit4/src/MyPackage/Test.java
+++ Junit4/src/MyPackage/Test.java
@@ -48,7 +48,7 @@ public class Test {
 	public void test() {
 		try {
 			System.out.println("in test method");
-			eyes.open(driver, AppName, TestName, new RectangleSize(900, 600));
+			eyes.open(driver, AppName, TestName, new RectangleSize(800, 600));
 			driver.get("https://applitools.com/helloworld/");
 			
 			eyes.checkWindow();
