a291c0408a8fdeee0fc4c1ff9db7b05284084d31
Sophie Tagar <sophie24587@gmail.com>
2018-06-12 14:32:16 -0700

    Update NewTest.java

--- TestNG/src/MyPackage/NewTest.java
+++ TestNG/src/MyPackage/NewTest.java
@@ -47,7 +47,7 @@ public class NewTest {
 		try {
 			System.out.println("in test method");
 			eyes.open(driver, AppName, TestName, new RectangleSize(800, 600));
-			driver.get("https://applitools.com/helloworld/?diff1");
+			driver.get("https://applitools.com/helloworld");
 
 			
 			eyes.checkWindow();
