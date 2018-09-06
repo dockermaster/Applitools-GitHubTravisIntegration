901b2e3349def2743c6c48190bf607db48dc5ecf
Sophie Tagar <sophie24587@gmail.com>
2018-09-06 15:04:15 -0700

    Update NewTest.java

--- TestNG/src/MyPackage/NewTest.java
+++ TestNG/src/MyPackage/NewTest.java
@@ -16,7 +16,7 @@ import com.applitools.eyes.selenium.fluent.Target;
 public class NewTest {
 	public Eyes eyes = new Eyes();
 	public String AppName = "GithubIntegration";
-	public String TestName = "Test_Github_Integration4";  
+	public String TestName = "Test_Github_Integration5";  
 	RemoteWebDriver driver;
 
 	
